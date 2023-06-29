fdffe56f48b47b239d195bb044e30e3f14f289e1
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-06-29 12:19:23 -0400

    added solution to exercise about scrabble points

---
+++ 31208_scrabble.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int letter_to_value(char letter) {
+    char *data[] = {
+        "",
+        "AEILNORSTU",
+        "DG",
+        "BCMP",
+        "FHVWY",
+        "K",
+        "",
+        "",
+        "JX",
+        "",
+        "QZ"
+    };
+    for (int i = 0; i <= 10; i++) {
+        char *p = data[i];
+        while (*p != '\0') {
+            if (toupper(letter) == *p) {
+                return i;
+            }
+            p++;
+        }
+    }
+    printf("Encountered an unknown character '%c'.\n", letter);
+    return 0;
+}
+
+int compute_scrabble_value(const char *word) {
+    int points = 0;
+    const char *p = word;
+    while (*p != '\0') {
+        points += letter_to_value(*p);
+        p++;
+    }
+    return points;
+}
+
+
+int main (void) {
+    char word[40];
+    char *p = &word[0];
+    printf("Enter a word: ");
+    while (true) {
+        char c = (char)(getchar());
+        if (c == '\n') {
+            break;
+        }
+        *p = c;
+        p++;
+    }
+    int points = compute_scrabble_value(word);
+    printf("Scrabble value: %d\n", points);
+    return EXIT_SUCCESS;
+}
