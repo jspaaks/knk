ee3687b879cfba5a203aba28ed9839dec94af0e6
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-04-19 13:53:54 -0400

    minor style changes

--- 37501a-justify/line.c
+++ 37501a-justify/line.c
@@ -30,18 +30,23 @@ int space_remaining (void) {
 }
 
 void write_line (void) {
-    int extra_spaces, spaces_to_insert, i, j;
+
+    int extra_spaces;
+    int spaces_to_insert
+    int i;
+    int j;
+
     extra_spaces = MAX_LINE_LEN - line_len;
     for (i = 0; i < line_len; i++) {
-        if (line[i] != ' ') {
-            putchar(line[i]);
-        } else {
+        if (line[i] == ' ') {
             spaces_to_insert = extra_spaces / (num_words - 1);
             for (j = 1; j <= spaces_to_insert + 1; j++) {
                 putchar(' ');
             }
             extra_spaces -= spaces_to_insert;
             num_words--;
+        } else {
+            putchar(line[i]);
         }
     }
     putchar('\n');
