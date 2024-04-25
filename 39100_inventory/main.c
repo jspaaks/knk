61470a247e320f6b8eedd43b9692d05ec9849f5b
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-04-25 13:30:01 -0400

    added inventory code snippet from p 391

--- 39100_inventory/main.c
+++ 39100_inventory/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include "readline.h"
+#include "readline.h"
 
 #define NAME_LEN 25
 #define MAX_PARTS 100
@@ -33,6 +33,27 @@ int find_part (int number) {
 
 
 void insert (void) {
+    int part_number;
+
+    if (num_parts == MAX_PARTS) {
+        printf("Database is full; Can't add any more parts.\n");
+        return;
+    }
+
+    printf("Enter part number: ");
+    scanf("%d", &part_number);
+
+    if (find_part(part_number) >= 0) {
+        printf("Part already exists.\n");
+        return;
+    }
+
+    inventory[num_parts].number = part_number;
+    printf("Enter part name: ");
+    read_line(inventory[num_parts].name, NAME_LEN);
+    printf("Enter quantity on hand: ");
+    scanf("%d", &inventory[num_parts].on_hand);
+    num_parts++;
 }
 
 int main (void) {
@@ -74,13 +95,41 @@ int main (void) {
 
 
 void search (void) {
+    int i, number;
+    printf("Enter part number: ");
+    scanf("%d", &number);
+    i = find_part(number);
+    if (i >= 0) {
+        printf("Part name: %s\n", inventory[i].name);
+        printf("Quantity on hand: %d\n", inventory[i].on_hand);
+    } else {
+        printf("Part not found.\n");
+    }
 }
 
 
 void update (void) {
+    int i, number, change;
+    printf("Enter part number: ");
+    scanf("%d", &number);
+    i = find_part(number);
+    if (i >= 0) {
+        printf("Enter change in quantity on hand: ");
+        scanf("%d", &change);
+        inventory[i].on_hand += change;
+    } else {
+        printf("Part not found.\n");
+    }
 }
 
 
 void print (void) {
+    int i;
+    printf("Part Number   Part Name                          "
+           "Quantity on Hand\n");
+    for (i = 0; i < num_parts; i++) {
+        printf("%7d       %-25s%11d\n", inventory[i].number,
+               inventory[i].name, inventory[i].on_hand);
+    }
 }
 
