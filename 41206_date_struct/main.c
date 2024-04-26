e16bc513471ec726408716aa0d73ec3ef2f5fc85
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-04-26 16:27:48 -0400

    updated assertions

--- 41206_date_struct/main.c
+++ 41206_date_struct/main.c
@@ -2,6 +2,14 @@
 #include <assert.h>
 #include "dates.h"
 
+void get_date (Date *); 
+
+
+void get_date (Date * d) {
+    scanf(" %2d / %2d / %4d", &(*d).month, &(*d).day, &(*d).year);
+    assert(1 <= (*d).day && (*d).day <= 31 && "day integer not from range [1..31]");
+    assert(1 <= (*d).month && (*d).month <= 12 && "month integer not from range [1..12]");
+}
 
 
 int main (void) {
@@ -10,16 +18,12 @@ int main (void) {
     Date d2;
 
     printf("Enter first date (mm/dd/yyyy): ");
-    scanf(" %2d / %2d / %4d", &d1.month, &d1.day, &d1.year);
-    printf("Enter second date (mm/dd/yyyy): ");
-    scanf(" %2d / %2d / %4d", &d2.month, &d2.day, &d2.year);
-
-    assert(1 <= d1.day && d1.day <= 31 && "day integer not from range [1..31]");
-    assert(1 <= d2.day && d2.day <= 31 && "day integer not from range [1..31]");
-    assert(1 <= d1.month && d1.month <= 31 && "month integer not from range [1..12]");
-    assert(1 <= d2.month && d2.month <= 31 && "month integer not from range [1..12]");
-
+    get_date(&d1);
     int doy1 = day_of_year(d1);
+
+
+    printf("Enter second date (mm/dd/yyyy): ");
+    get_date(&d2);
     int doy2 = day_of_year(d2);
 
     printf("day of year for %d-%d-%d is: %d\n", d1.day, d1.month, d1.year, doy1);
