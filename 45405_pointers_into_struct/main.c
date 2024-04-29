b32eaef82b7dd7567238f7dd137477676ed2954c
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-04-29 14:10:45 -0400

    added solution to exercise about pointers into struct syntax

---
+++ 45405_pointers_into_struct/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main (void) {
+    struct {
+        union {
+            char a;
+            char b;
+            int c;
+        } d;
+        int e[5];
+    } f, *p = &f;
+
+    // a) p->b invalid need to go through union member first p->d.b
+    printf("a) p->b = ' ';      invalid, use p->d.b =  ' ';\n");
+    p->d.b =  ' ';
+
+    // b) valid
+    printf("b) p->e[3];         valid\n");
+    p->e[3] = 10;
+
+    // c) valid
+    printf("c) (*p).d.a;        valid\n");
+    (*p).d.a = '*';
+
+    // d) invalid, d is not a pointer. 
+    printf("d) p->d->c = 20;    invalid, use p->d.c = 20;\n");
+    p->d.c = 20;
+
+}
