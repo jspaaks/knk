b615f6c5ff719cceea196c01fb8705f67e2112d2
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-08-05 12:28:04 -0400

    updated the printed names for both queues in main.c

--- 50707_queue_adt_linked_list_void_pointer/src/queueclient/main.c
+++ 50707_queue_adt_linked_list_void_pointer/src/queueclient/main.c
@@ -18,33 +18,33 @@ int main (void) {
     };
 
     fprintf(stdout, "Create queue named 'Queue[int]'.\n");
-    Queue * q1 = queue__create("q1");
+    Queue * q1 = queue__create("Queue[int]");
     queue__print_state(q1);
     fprintf(stdout, "\n");
 
     // ---
 
     fprintf(stdout, "Create queue named 'Queue[str]'.\n");
-    Queue * q2 = queue__create("q2");
+    Queue * q2 = queue__create("Queue[str]");
     queue__print_state(q2);
     fprintf(stdout, "\n");
 
     // ---
 
-    fprintf(stdout, "Add 100 to q1\n");
+    fprintf(stdout, "Add 100 to Queue[int]\n");
     queue__join(q1, (void *) &data.ints[0]);
     queue__print_state(q1);
     fprintf(stdout, "\n");
 
     // ---
 
-    fprintf(stdout, "Pop the first item off q1.\n");
+    fprintf(stdout, "Pop the first item off Queue[int].\n");
     void * popped = NULL;
     bool haderr = queue__pop(q1, &popped);
     if (!haderr) {
-        fprintf(stdout, " -- Popped item from q1 was %3d\n", *((int *)(popped)));
+        fprintf(stdout, " -- Popped item from Queue[int] was %3d\n", *((int *)(popped)));
     } else {
-        fprintf(stderr, " -- There was an error popping an element from q1\n");
+        fprintf(stderr, " -- There was an error popping an element from Queue[int]\n");
     }
     queue__print_state(q1);
     fprintf(stdout, "\n");
@@ -52,7 +52,7 @@ int main (void) {
     // ---
 
     for (size_t i = 0; i < 3; i++) {
-        fprintf(stdout, "Item '%s' joins q2\n", data.strings[i]);
+        fprintf(stdout, "Item '%s' joins Queue[str]\n", data.strings[i]);
         queue__join(q2, (void *) data.strings[i]);
         queue__print_state(q2);
         fprintf(stdout, "\n");
@@ -60,28 +60,28 @@ int main (void) {
 
     // ---
 
-    fprintf(stdout, "Time to put q1 out to pasture\n");
+    fprintf(stdout, "Time to put Queue[int] out to pasture\n");
     queue__destroy(&q1);
     fprintf(stdout, "\n");
 
     // ---
 
-    fprintf(stdout, "Pop items off q2.\n");
+    fprintf(stdout, "Pop items off Queue[str].\n");
     size_t nact = queue__get_length(q2);
     for (size_t i = 0; i < nact; i++) {
         void * popped = NULL;
         bool haderr = queue__pop(q2, &popped);
         if (!haderr) {
-            fprintf(stdout, " -- Popped item from q2 was '%s'\n", (char *) popped);
+            fprintf(stdout, " -- Popped item from Queue[str] was '%s'\n", (char *) popped);
         } else {
-            fprintf(stderr, " -- There was an error popping an element from q2\n");
+            fprintf(stderr, " -- There was an error popping an element from Queue[str]\n");
         }
         queue__print_state(q2);
         fprintf(stdout, "\n");
     }
     // ---
 
-    fprintf(stdout, "Time to put q2 out to pasture\n");
+    fprintf(stdout, "Time to put Queue[str] out to pasture\n");
     queue__destroy(&q2);
 
     // ---
