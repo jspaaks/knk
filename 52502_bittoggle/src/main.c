4c0529196815899ec219aa3a3bf450e2ed377ba3
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-08-15 13:19:49 -0400

    added a \0 for safer printing; added a README

--- 52502_bittoggle/src/main.c
+++ 52502_bittoggle/src/main.c
@@ -3,16 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printbits (uint8_t v, char (*buf)[8]) {
+void printbits (const uint8_t v, char (*buf)[9]) {
     for (size_t i = 0; i < 8; ++i) {
         (*buf)[7 - i] = v & (1 << i) ? '1' : '0';
     }
+    (*buf)[8] = '\0';
 }
 
-void toggle (uint8_t i, uint8_t ibit) {
+void toggle (const uint8_t ibit, uint8_t i) {
     assert(ibit < 8 && "Didn't expect ibit values larger than 7.");
 
-    char bits[8] = {};
+    char bits[9] = {};
     printbits(i, &bits);
     fprintf(stdout, "i = %d (0b%s)\n", i, bits);
 
@@ -25,14 +26,14 @@ void toggle (uint8_t i, uint8_t ibit) {
 }
 
 int main (void) {
-    toggle(255, 7);
-    toggle(127, 6);
-    toggle(63, 5);
-    toggle(31, 4);
-    toggle(15, 3);
-    toggle(7, 2);
-    toggle(3, 1);
-    toggle(1, 0);
-    toggle(0, 7);
+    toggle(7, 255);
+    toggle(6, 127);
+    toggle(5, 63);
+    toggle(4, 31);
+    toggle(3, 15);
+    toggle(2, 7);
+    toggle(1, 3);
+    toggle(0, 1);
+    toggle(7, 0);
     return EXIT_SUCCESS;
 }
