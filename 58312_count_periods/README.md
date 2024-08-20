3ee5d15929fe67bd2872fd10d2189e95d59e1e86
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-08-20 10:52:20 -0400

    added solution to exercise about counting periods in a user-supplied file

---
+++ 58312_count_periods/README.md
@@ -0,0 +1,17 @@
+# 58312_count_periods
+
+```shell
+mkdir build
+cd build
+cmake ..
+cmake --build .
+cmake --install .
+echo "asdasd.asdas.d.asd.sd...asd" > periods.txt
+./dist/bin/count-periods periods.txt
+```
+
+Should output something like:
+
+```text
+7
+```
---
+++ 58312_count_periods/src/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int count_periods (const char * filename) {
+    int n = 0;
+    FILE * fp = fopen(filename, "r");
+    if (fp != NULL) {
+        while (true) {
+            int c = fgetc(fp);
+            if (c == EOF) break;
+            if (c == '.') n++;
+        }
+        fclose(fp);
+    }
+    return n;
+}
+
+int main (int argc, char * argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: count-periods FILENAME\n");
+        fprintf(stderr, "Return the number of periods in a given string\n");
+    }
+
+    int n = count_periods(argv[1]);
+    fprintf(stdout, "%d\n", n);
+
+    return EXIT_SUCCESS;
+}
