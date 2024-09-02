5abce563008e701bcf57c50a30656153c4b96859
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-09-02 13:18:23 -0400

    put filename and its FILE * into a new typedef struct

--- 58816_fcopy_in_blocks/src/main.c
+++ 58816_fcopy_in_blocks/src/main.c
@@ -3,26 +3,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main (int argc, char * argv[]) {
+typedef struct file {
+    FILE * fp;
+    char * filename;
+} File;
 
-    FILE * source_fp;
-    FILE * dest_fp;
+int main (int argc, char * argv[]) {
 
     if (argc != 3) {
         fprintf(stderr, "usage fcopy source dest\n");
         exit(EXIT_FAILURE);
     }
 
-    source_fp = fopen(argv[1], "rb");
-    if (source_fp == nullptr) {
-        fprintf(stderr, "Can't open file '%s'.\n", argv[1]);
+    File src = {
+        .filename = argv[1],
+        .fp = fopen(argv[1], "rb"),
+    };
+    if (src.fp == nullptr) {
+        fprintf(stderr, "Can't open file '%s': %s.\n", src.filename, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    dest_fp = fopen(argv[2], "wb");
-    if (dest_fp == nullptr) {
-        fprintf(stderr, "Can't open file '%s'.\n", argv[2]);
-        fclose(source_fp);
+    File dst = {
+        .filename = argv[2],
+        .fp = fopen(argv[2], "wb"),
+    };
+    if (dst.fp == nullptr) {
+        fprintf(stderr, "Can't open file '%s': %s.\n", dst.filename, strerror(errno));
+        fclose(src.fp);
         exit(EXIT_FAILURE);
     }
 
@@ -32,26 +40,26 @@ int main (int argc, char * argv[]) {
     unsigned int nwritten;
 
     while (true) {
-        nread = fread(buf, 1, bsize, source_fp);
-        if (nread < bsize && ferror(source_fp)) {
-            fprintf(stderr, "Error reading from file '%s': %s", argv[1], strerror(errno));
+        nread = fread(buf, 1, bsize, src.fp);
+        if (nread < bsize && ferror(src.fp)) {
+            fprintf(stderr, "Error reading from file '%s': %s.\n", src.filename, strerror(errno));
             errno = 0;
             exit(EXIT_FAILURE);
         }
-        if (nread < bsize && feof(source_fp)) {
-            nwritten = fwrite(buf, 1, nread, dest_fp);
-            if (nwritten < nread && ferror(dest_fp)) {
-                fprintf(stderr, "Error writing to file '%s': %s", argv[2], strerror(errno));
+        if (nread < bsize && feof(src.fp)) {
+            nwritten = fwrite(buf, 1, nread, dst.fp);
+            if (nwritten < nread && ferror(dst.fp)) {
+                fprintf(stderr, "Error writing to file '%s': %s.\n", dst.filename, strerror(errno));
                 errno = 0;
                 exit(EXIT_FAILURE);
             }
             break;
         }
-        fwrite(buf, 1, bsize, dest_fp);
+        fwrite(buf, 1, bsize, dst.fp);
     }
 
-    fclose(source_fp);
-    fclose(dest_fp);
+    fclose(src.fp);
+    fclose(dst.fp);
 
     return EXIT_SUCCESS;
 }
