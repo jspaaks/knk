0750bd80e204830be382324cfa7fe6d91fd979c3
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-09-12 16:58:29 -0400

    added solution to exercise about trigraphs + iso646 -- just buy a keyboard already

---
+++ 67607_trigraphs_made_worse/README.md
@@ -0,0 +1,32 @@
+# 67607_trigraphs_made_worse
+
+Bleeding trigraphs and iso646 why?!
+
+## Build and install
+
+```console
+mkdir build
+cd build
+cmake ..
+cmake --build .
+cmake --install .
+```
+
+## Prep
+
+```shell
+$ cat ../plain.txt
+Trust not him with your secrets, who, when left
+alone in your room, turns over your papers.
+            --Johann Kaspar Lavater (1741-1801)
+```
+
+## Run
+
+```console
+$ ./dist/bin/trigraphs <../plain.txt
+rTSUR HIR NOK QORN _IST UCETCRU, QNI, QNCH JC@R
+GJIHC OH _IST TIIK, RSTHU IPCT _IST VGVCTU.
+            --lINGHH mGUVGT jGPGRCT (1741-1801)
+```
+
---
+++ 67607_trigraphs_made_worse/src/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <iso646.h>
+
+#define KEY '&'
+
+void show_usage (FILE * stream);
+
+int main (int argc, char * argv[]) {
+
+    if (argc == 2 && (strncmp(argv[1], "--help", 7) == 0 || strncmp(argv[1], "-h", 3) == 0)) {
+        show_usage(stdout);
+        exit(EXIT_SUCCESS);
+    }
+
+    if (argc != 1) {
+        show_usage(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    char orig_char;
+    char new_char;
+
+    /*
+    while ((orig_char = getchar()) != EOF) {
+        new_char = orig_char ^ KEY;
+        if (isprint(orig_char) && isprint(new_char))
+            putchar(new_char);
+        else
+            putchar(orig_char);
+    }
+    */
+
+    while ((orig_char = getchar()) not_eq EOF) ??<
+        new_char = orig_char ??' KEY;
+        if (isprint(orig_char) and isprint(new_char))
+            putchar(new_char);
+        else
+            putchar(orig_char);
+    ??>
+    return EXIT_SUCCESS;
+}
+
+void show_usage (FILE * stream) {
+    char * exename = "trigraphs";
+    fprintf(stream,
+            "Usage: %s\n"
+            "    Program to demonstrate trigraphs.\n",
+            exename);
+}
+
