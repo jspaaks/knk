faf49dc957f5744ad0973cd9d2db01ee1ed86e74
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-09-16 15:01:56 -0400

    added solution to exercise about function with variable number of arguments

---
+++ 70201_max_int_va_list/README.md
@@ -0,0 +1,23 @@
+# 70201_max_int_va_list
+
+## Build and install
+
+```console
+mkdir build
+cd build
+cmake ..
+cmake --build .
+cmake --install .
+```
+
+## Run
+
+```console
+$ ./dist/bin/max_int --help
+Usage: max_int
+    Demo variable number of function arguments.
+
+$ ./dist/bin/max_int
+Largest int in sequence is: 3145.
+```
+
---
+++ 70201_max_int_va_list/src/main.c
@@ -0,0 +1,47 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int max_int (...);
+void show_usage (FILE * stream);
+
+int main (int argc, char * argv[]) {
+
+    if (argc == 2 && (strncmp(argv[1], "--help", 7) == 0 || strncmp(argv[1], "-h", 3) == 0)) {
+        show_usage(stdout);
+        exit(EXIT_SUCCESS);
+    }
+
+    if (argc != 1) {
+        show_usage(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(stdout, "Largest int in sequence is: %d.\n", max_int(124, 56, 735, 687, 3145, 24, 56, 0));
+
+    return EXIT_SUCCESS;
+}
+
+int max_int (...) {
+    va_list ap;
+    va_start(ap, n);
+    int largest = va_arg(ap, int);
+    int current = largest;
+    while (current != 0) {
+        current = va_arg(ap, int);
+        if (current > largest) {
+            largest = current;
+        }
+    }
+    va_end(ap);
+    return largest;
+}
+
+void show_usage (FILE * stream) {
+    char * exename = "max_int";
+    fprintf(stream,
+            "Usage: %s\n"
+            "    Demo variable number of function arguments.\n",
+            exename);
+}
